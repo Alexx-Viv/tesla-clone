3c32851b59c11bd6f883d540551f71aa7d9f0eb8
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2022-01-24 18:33:35 +0100

    Add sections

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,16 +1,78 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-function Section() {
-  return <Wrap>
-    Section
-  </Wrap>;
+function Section({title, description, leftBtnText, rightBtnText, backgroundImg}) {
+  return (
+    <Wrap bgImage={backgroundImg}>
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          { rightBtnText && 
+          <RightButton>{rightBtnText}</RightButton>
+          }
+        </ButtonGroup>
+        <DownArrow src='/images/down-arrow.svg' />
+      </Buttons>
+    </Wrap>
+  )
 }
 
-export default Section;
+export default Section
 
 const Wrap = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background-color: orange;
+  width: 100vw;
+  height: 100vh;
+  background-image: ${props => `url(/images/${props.bgImage})`};
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  justify-content: center;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 265px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  margin: 8px;
+`
+const RightButton = styled(LeftButton)`
+  background-color: white;
+  opacity: 0.65;
+  color: black;
+`
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  overflow-y: hidden;
+  animation: animationDown infinite 1.5s;
+`
+const Buttons = styled.div``
