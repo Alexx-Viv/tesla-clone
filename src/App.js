b0baa680f09f6858970edb5a60a2a9b8f4714d7b
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2022-01-28 16:01:42 +0100

    Almost finish homepage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import Header from './components/Header.js';
-import Home from './components/Home.js';
+import React from 'react'
+import './App.css'
+import Header from './components/Header.js'
+import Home from './components/Home.js'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
       <Home />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
